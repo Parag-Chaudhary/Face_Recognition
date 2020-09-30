5632fe65ce047ce99a16fb9422880c19438ed0de
Parag-Chaudhary <70112406+Parag-Chaudhary@users.noreply.github.com>
2020-10-01 04:00:35 +0530

    Update Encoder.py

--- Encoder.py
+++ Encoder.py
@@ -1,8 +1,10 @@
+# Importing Modules
 import face_recognition
 import pickle
 import cv2
 import os
 
+# Setting the working Directory
 DIRECTORY = r'D:\Coursera\MLProject\Face Recoginition\Datasets'
 CATERGORIES = os.listdir(DIRECTORY)
 
@@ -19,20 +21,11 @@ for names in CATERGORIES:
 
         image = face_recognition.load_image_file(img_path)
         color = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+# Detecting the faces
         face_locations = face_recognition.face_locations(color)
+# Making Encodings
         encodings = face_recognition.face_encodings(color,face_locations)[0]
-        #(BECAUSE WE WANT THAT EVERY IMAGE ENCODINGS MUST BE AT 0 INDEX OF encodings SO THAT SINGLE
-        # ENCODING WILL APPEND IN THIS knownEncodings....BUT IS DONT USE [0] THEN NEXT ENCODINGS WILL AT
-        # THE NEXT POSTION OF THE encodings WHILE PREVIOUS WILL THERE ALSO)
-
-        # # loop over the encodings  ( ALSO DO THIS IF WE DID NOT WANT [0] )
-        # for encoding in encodings:
-        #     # add each encoding + name to our set of known names and
-        #     # encodings
-        #     knownEncodings.append(encoding)
-        #     knownNames.append(name)
-
+# Saving the Encodings and the respective name     
         KNOWN_ENCODINGS.append(encodings)
         KNOWN_NAMES.append(names)
 
